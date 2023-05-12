5320fb2b38644aa88eb15f765039bc1f599b52fe
mikemutuma <mikemutuma664@gmail.com>
2023-05-12 16:02:44 -0700

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int aa, b, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,13 +20,13 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	aa = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	b = write(aa, text_content, len);
 
-	if (fd == -1 || w == -1)
+	if (aa == -1 || b == -1)
 		return (-1);
 
-	close(fd);
+	close(aa);
 
 	return (1);
 }
